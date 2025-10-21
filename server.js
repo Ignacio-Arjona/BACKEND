3d90a93cbfd77e278c53c91b62579093a20d9f2b
Ignacio Arjona <9400472@alumnos.ufv.es>
2025-10-21 21:51:25 +0200

    version3

--- server.js
+++ server.js
@@ -24,4 +24,42 @@ app.post('/signup', async (req, res) => {
   res.send('User saved!');
 });
 
+
+
+app.post('/login', async (req, res) => {
+  const { username, password } = req.body;
+  const user = await User.findOne({ username, password });
+  if (user) {
+    res.send('Login successful!');
+  } else {
+    res.status(401).send('Invalid credentials');
+  }
+});
+
+app.delete('/delete', async (req, res) => {
+  const { username, password } = req.body;
+  const result = await User.deleteOne({ username, password });
+  if (result.deletedCount > 0) {
+    res.send('Account deleted');
+  } else {
+    res.status(404).send('User not found');
+  }
+});
+
+app.put('/change-password', async (req, res) => {
+  const { username, oldPassword, newPassword } = req.body;
+  const user = await User.findOne({ username, password: oldPassword });
+  if (user) {
+    user.password = newPassword;
+    await user.save();
+    res.send('Password updated');
+  } else {
+    res.status(401).send('Invalid credentials');
+  }
+});
+
+app.post('/logout', (req, res) => {
+  res.send('Logged out');
+});
+
 app.listen(3000, () => console.log('Server running on port 3000'));
